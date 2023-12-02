635ffcd249785f7956a365ea31dce04441c8e365
Gustavo Diaz Galeas <diazg.gustavoa@gmail.com>
2023-12-01 23:21:21 -0500

    Soln implementation of Part 1

--- src/day1/trebuchet.cpp
+++ src/day1/trebuchet.cpp
@@ -1,8 +1,108 @@
+#include <array>
+#include <cctype>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main(void)
+#include <errno.h>
+
+// Debug purposes
+// #define ENABLE_DEBUG
+
+#ifdef ENABLE_DEBUG
+#include <filesystem>
+
+void print_cwd(void)
 {
-    std::cout << "Hello, world!\n";
+    std::filesystem::path cwd = std::filesystem::current_path();
+    std::cerr << "Current working directory: "
+        << cwd << "\n";
+}
+#endif
+
+// IDEA: make use of an "acc" to keep track of overall
+// calibration value.
+//
+// Have a function that exctracts the values from each line
+// and adds it to the "acc"
+//
+// NOTE: each line contains a calibration value that can be obtained
+//      by combining the FIRST and the LAST digit (in that order) to
+//      form a single two-digit number
+int obtain_val(const std::array<int, 2> &d)
+{
+    int num{ };
+
+    for (const int& n : d)
+    {
+        num = num * 10 + n;
+    }
+
+    return num;
+}
+
+int extract_calibration_val(std::string_view sv)
+{
+    std::array<int, 2> digits{ -1, -1 };
+
+    for (const char& c : sv)
+    {
+        if (isdigit(c))
+        {
+            // Populate with first encountered digit
+            if (digits[0] == -1)
+            {
+                digits[0] = c - '0';
+                digits[1] = c - '0';
+            }
+            else
+            {
+                digits[1] = c - '0';
+            }
+        }
+    }
+
+    std::cout << "Extracted number: " << digits[0] << digits[1] << "\n";
+
+    return obtain_val(digits);
+}
+
+int main(int argc, char* argv[])
+{
+#ifdef ENABLE_DEBUG
+print_cwd();
+#endif
+
+    // Must include an input file as an arg
+    if (argc != 2)
+    {
+        std::cerr << "Error!\n";
+        std::cerr << "\tExpected form: Trebuchet.exe [name of input file].txt\n\n";
+        std::cerr << "\tTerminating...\n";
+
+        return EINVAL;
+    }
+
+    std::stringstream file_name{ argv[1] };
+    std::ifstream inf{ file_name.str() };
+    int acc{ };
+
+    // Check if file was able to be opened
+    if (!inf)
+    {
+        std::cerr << "Uh oh, sample-puzzle.txt could not be opened for reading!\n";
+        return ENOENT;
+    }
+
+    // Read from file, line by line
+    std::string line;
+    while (std::getline(inf, line))
+    {
+        acc += extract_calibration_val(line);
+    }
+
+    std::cout << "The sum of the calibration values is: " << acc << "\n";
 
     return 0;
 }
